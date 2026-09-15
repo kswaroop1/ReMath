188b641cb84bd313903a81bc988be009103c810e
kswaroop1 <kswaroop@gmail.com>
2026-09-15 18:01:18 +0100

    feat: add stable performance budget evaluation

---
+++ lib/src/core/performance_budget.dart
@@ -0,0 +1,55 @@
+final class PerformanceBudget {
+  PerformanceBudget({
+    required this.name,
+    required this.maximum,
+    required this.warmUpRuns,
+    required this.sampleRuns,
+  }) {
+    if (sampleRuns < 3 || sampleRuns.isEven) {
+      throw ArgumentError.value(
+        sampleRuns,
+        'sampleRuns',
+        'must be an odd number of at least three samples',
+      );
+    }
+  }
+
+  final String name;
+  final Duration maximum;
+  final int warmUpRuns;
+  final int sampleRuns;
+
+  PerformanceBudgetResult evaluate(List<Duration> samples) {
+    if (samples.length != sampleRuns) {
+      throw ArgumentError.value(
+        samples.length,
+        'samples',
+        'must contain exactly $sampleRuns measurements',
+      );
+    }
+    final ordered = [...samples]..sort();
+    return PerformanceBudgetResult(
+      name: name,
+      maximum: maximum,
+      median: ordered[ordered.length ~/ 2],
+    );
+  }
+}
+
+final class PerformanceBudgetResult {
+  const PerformanceBudgetResult({
+    required this.name,
+    required this.maximum,
+    required this.median,
+  });
+
+  final String name;
+  final Duration maximum;
+  final Duration median;
+
+  bool get isWithinBudget => median <= maximum;
+
+  String get failureMessage =>
+      '$name median ${median.inMilliseconds}ms exceeded allowed '
+      '${maximum.inMilliseconds}ms';
+}
