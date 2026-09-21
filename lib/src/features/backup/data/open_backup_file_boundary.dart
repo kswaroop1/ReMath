0d58b103a0fcb016f0e7527e34439ae982bcd6e3
kswaroop1 <kswaroop@gmail.com>
2026-09-21 10:31:44 +0100

    style: stabilize platform boundary formatting

--- lib/src/features/backup/data/open_backup_file_boundary.dart
+++ lib/src/features/backup/data/open_backup_file_boundary.dart
@@ -2,4 +2,6 @@ import '../application/backup_file_transfer.dart';
 import 'open_backup_file_boundary_stub.dart'
     if (dart.library.io) 'open_backup_file_boundary_io.dart' as platform;
 
-BackupFileBoundary? openBackupFileBoundary() => platform.openBackupFileBoundary();
+BackupFileBoundary? openBackupFileBoundary() {
+  return platform.openBackupFileBoundary();
+}
