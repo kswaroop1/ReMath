f0ec30797a940f384ba65b4c2e17dcca84f85ced
kswaroop1 <kswaroop@gmail.com>
2026-09-21 09:19:28 +0100

    style: apply CI formatter to backup boundary

--- lib/src/features/backup/data/open_backup_file_boundary.dart
+++ lib/src/features/backup/data/open_backup_file_boundary.dart
@@ -2,5 +2,4 @@ import '../application/backup_file_transfer.dart';
 import 'open_backup_file_boundary_stub.dart'
     if (dart.library.io) 'open_backup_file_boundary_io.dart' as platform;
 
-BackupFileBoundary? openBackupFileBoundary() =>
-    platform.openBackupFileBoundary();
+BackupFileBoundary? openBackupFileBoundary() => platform.openBackupFileBoundary();
